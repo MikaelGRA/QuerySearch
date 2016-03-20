208d210f3bc661a3a0b550807189168b79c2899e
MikaelGRA <mikaelgra@gmail.com>
2016-03-20 14:59:33 +0100

    added missing files

---
+++ src/Vibrant.QuerySearch.Unity/UnityContainerExtensions.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace Vibrant.QuerySearch.Unity
+{
+   /// <summary>
+   /// Simple extensions for making working with QuerySearch and Unity easier.
+   /// </summary>
+   public static class UnityContainerExtensions
+   {
+      /// <summary>
+      /// Registers a localization service.
+      /// </summary>
+      /// <typeparam name="TLocalizationService"></typeparam>
+      /// <param name="registry"></param>
+      /// <param name="lifetimeManager"></param>
+      public static void RegisterLocalizationService<TLocalizationService>(
+         this IUnityContainer registry,
+         LifetimeManager lifetimeManager )
+         where TLocalizationService : ILocalizationService
+      {
+         registry.RegisterType<ILocalizationService, TLocalizationService>( lifetimeManager );
+      }
+
+      /// <summary>
+      /// Registers a localization service.
+      /// </summary>
+      /// <typeparam name="TLocalizationService"></typeparam>
+      /// <param name="registry"></param>
+      public static void RegisterLocalizationService<TLocalizationService>(
+         this IUnityContainer registry )
+         where TLocalizationService : ILocalizationService
+      {
+         registry.RegisterType<ILocalizationService, TLocalizationService>();
+      }
+
+      /// <summary>
+      /// Registers a filter provider.
+      /// </summary>
+      /// <typeparam name="TEntity"></typeparam>
+      /// <typeparam name="TFilterProvider"></typeparam>
+      /// <param name="registry"></param>
+      /// <param name="lifetimeManager"></param>
+      public static void RegisterFilterProvider<TEntity, TFilterProvider>(
+         this IUnityContainer registry,
+         LifetimeManager lifetimeManager )
+         where TFilterProvider : IFilterProvider<TEntity>
+      {
+         registry.RegisterType<IFilterProvider<TEntity>, TFilterProvider>( lifetimeManager );
+      }
+
+      /// <summary>
+      /// Registers a filter provider.
+      /// </summary>
+      /// <typeparam name="TEntity"></typeparam>
+      /// <typeparam name="TFilterProvider"></typeparam>
+      /// <param name="registry"></param>
+      public static void RegisterFilterProvider<TEntity, TFilterProvider>(
+         this IUnityContainer registry )
+         where TFilterProvider : IFilterProvider<TEntity>
+      {
+         registry.RegisterType<IFilterProvider<TEntity>, TFilterProvider>();
+      }
+
+      /// <summary>
+      /// Registers a pagination provider.
+      /// </summary>
+      /// <typeparam name="TEntity"></typeparam>
+      /// <typeparam name="TPaginationProvider"></typeparam>
+      /// <param name="registry"></param>
+      /// <param name="lifetimeManager"></param>
+      public static void RegisterPaginationProvider<TEntity, TPaginationProvider>(
+         this IUnityContainer registry,
+         LifetimeManager lifetimeManager )
+         where TPaginationProvider : IPaginationProvider<TEntity>
+      {
+         registry.RegisterType<IPaginationProvider<TEntity>, TPaginationProvider>( lifetimeManager );
+      }
+
+      /// <summary>
+      /// Registers a pagination provider.
+      /// </summary>
+      /// <typeparam name="TEntity"></typeparam>
+      /// <typeparam name="TPaginationProvider"></typeparam>
+      /// <param name="registry"></param>
+      public static void RegisterPaginationProvider<TEntity, TPaginationProvider>(
+         this IUnityContainer registry )
+         where TPaginationProvider : IPaginationProvider<TEntity>
+      {
+         registry.RegisterType<IPaginationProvider<TEntity>, TPaginationProvider>();
+      }
+   }
+}
---
+++ src/Vibrant.QuerySearch.Unity/UnityDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace Vibrant.QuerySearch.Unity
+{
+   /// <summary>
+   /// Unity implementation of IDependencyResolver using a UnityContainer behind the scenes.
+   /// </summary>
+   public class UnityDependencyResolver : IDependencyResolver
+   {
+      private readonly UnityContainer _container;
+
+      /// <summary>
+      /// Constructs a UnityDependencyResolver using a UnityContainer.
+      /// </summary>
+      /// <param name="container"></param>
+      public UnityDependencyResolver( UnityContainer container )
+      {
+         _container = container;
+      }
+
+      /// <summary>
+      /// Resolves an instance of the specified service.
+      /// </summary>
+      /// <typeparam name="TService"></typeparam>
+      /// <returns>An instance of the service to be resolved.</returns>
+      public TService Resolve<TService>()
+      {
+         return _container.Resolve<TService>();
+      }
+   }
+}
