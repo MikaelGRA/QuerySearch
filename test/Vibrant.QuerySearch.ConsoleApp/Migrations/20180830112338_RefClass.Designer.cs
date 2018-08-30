e5b19c8034270d63488c9c986c6962315e06ba7a
Jonas Mark Frost Pedersen <jmfp@insero.com>
2018-08-30 13:40:17 +0200

    fix to support include and test

--- test/Vibrant.QuerySearch.ConsoleApp/Migrations/20180830112338_RefClass.Designer.cs
+++ test/Vibrant.QuerySearch.ConsoleApp/Migrations/20180830112338_RefClass.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vibrant.QuerySearch.ConsoleApp;
 
 namespace Vibrant.QuerySearch.ConsoleApp.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180830112338_RefClass")]
+    partial class RefClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
